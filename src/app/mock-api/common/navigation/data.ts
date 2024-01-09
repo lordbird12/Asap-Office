4977e1e2022856a387aa5e4413c8be7eb1e6739c
info1090 <info@asha-tech.co.th>
2024-01-09 15:41:58 +0700

    first commit

---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,314 @@
+/* eslint-disable */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id: 'admin',
+        title: 'Admin',
+        subtitle: 'ขัอมูลเกี่ยวกับระบบ',
+        type: 'group',
+        icon: 'heroicons_outline:home',
+        children: [
+            {
+                id: 'admin.comp',
+                title: 'พนักงาน',
+                type: 'basic',
+                icon: 'heroicons_outline:list-bullet',
+                link: '/admin/employee/list',
+            },
+            {
+                id: 'admin.department',
+                title: 'ศูนย์บริการ',
+                type: 'basic',
+                icon: 'heroicons_outline:list-bullet',
+                link: '#',
+            },
+            {
+                id: 'admin.position',
+                title: 'รถ',
+                type: 'basic',
+                icon: 'heroicons_outline:list-bullet',
+                link: '#',
+            },
+            {
+                id: 'admin.employee',
+                title: 'บัญชีลูกค้า',
+                type: 'basic',
+                icon: 'heroicons_outline:list-bullet',
+                link: '/admin/client/list',
+            },
+            {
+                id: 'admin.permission',
+                title: 'ประวัติทำรายการ',
+                type: 'basic',
+                icon: 'heroicons_outline:list-bullet',
+                link: '#',
+            },
+        ],
+    },
+    {
+        id: 'callcenter',
+        title: 'Call Center',
+        subtitle: 'ขัอมูลเกี่ยวกับระบบ',
+        type: 'group',
+        icon: 'heroicons_outline:home',
+        children: [
+            {
+                id: 'admin.comp',
+                title: 'สรุปภาพรวม',
+                type: 'basic',
+                icon: 'heroicons_outline:list-bullet',
+                link: '/dashboards/project',
+            },
+            {
+                id: 'admin.department',
+                title: 'Ticket',
+                type: 'basic',
+                icon: 'heroicons_outline:list-bullet',
+                link: '#',
+            },
+            {
+                id: 'admin.department',
+                title: 'ศูนย์บริการ',
+                type: 'basic',
+                icon: 'heroicons_outline:list-bullet',
+                link: '#',
+            },
+            {
+                id: 'admin.position',
+                title: 'รถ',
+                type: 'basic',
+                icon: 'heroicons_outline:list-bullet',
+                link: '#',
+            },
+            {
+                id: 'admin.employee',
+                title: 'บัญชีลูกค้า',
+                type: 'basic',
+                icon: 'heroicons_outline:list-bullet',
+                link: '/admin/client/list',
+            },
+            {
+                id: 'admin.permission',
+                title: 'ประวัติทำรายการ',
+                type: 'basic',
+                icon: 'heroicons_outline:list-bullet',
+                link: '#',
+            },
+        ],
+    },
+    {
+        id: 'gm',
+        title: 'General Manager',
+        subtitle: 'ขัอมูลเกี่ยวกับระบบ',
+        type: 'group',
+        icon: 'heroicons_outline:home',
+        children: [
+            {
+                id: 'sales.list',
+                title: 'คำสั่งซื้อ',
+                type: 'basic',
+                icon: 'heroicons_outline:home-modern',
+                link: '/admin/sales/list',
+            },
+            {
+                id: 'client.list',
+                title: 'ลูกค้าของเรา',
+                type: 'basic',
+                icon: 'heroicons_outline:home-modern',
+                link: '/admin/customer/list',
+            },
+        ],
+    },
+    {
+        id: 'self',
+        title: 'User Profile',
+        subtitle: 'ขัอมูลเกี่ยวกับระบบ',
+        type: 'group',
+        icon: 'heroicons_outline:home',
+        children: [
+            {
+                id: 'self.employee',
+                title: 'แก้ไขข้อมูลส่วนตัว',
+                type: 'basic',
+                icon: 'heroicons_outline:user',
+                link: '/admin/employee/list',
+            },
+            {
+                id: 'admin.logout',
+                title: 'ออกจากระบบ',
+                type: 'basic',
+                icon: 'heroicons_outline:arrow-left-on-rectangle',
+                link: '/sign-out',
+            },
+        ],
+    },
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id: 'dashboards',
+        title: 'Dashboards',
+        tooltip: 'Dashboards',
+        type: 'aside',
+        icon: 'heroicons_outline:home',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'apps',
+        title: 'Apps',
+        tooltip: 'Apps',
+        type: 'aside',
+        icon: 'heroicons_outline:qr-code',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'pages',
+        title: 'Pages',
+        tooltip: 'Pages',
+        type: 'aside',
+        icon: 'heroicons_outline:document-duplicate',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'user-interface',
+        title: 'UI',
+        tooltip: 'UI',
+        type: 'aside',
+        icon: 'heroicons_outline:rectangle-stack',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'navigation-features',
+        title: 'Navigation',
+        tooltip: 'Navigation',
+        type: 'aside',
+        icon: 'heroicons_outline:bars-3',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id: 'dashboards',
+        title: 'DASHBOARDS',
+        type: 'group',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'apps',
+        title: 'APPS',
+        type: 'group',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'others',
+        title: 'OTHERS',
+        type: 'group',
+    },
+    {
+        id: 'pages',
+        title: 'Pages',
+        type: 'aside',
+        icon: 'heroicons_outline:document-duplicate',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'user-interface',
+        title: 'User Interface',
+        type: 'aside',
+        icon: 'heroicons_outline:rectangle-stack',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
+        id: 'navigation-features',
+        title: 'Navigation Features',
+        type: 'aside',
+        icon: 'heroicons_outline:bars-3',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id: 'dashboards',
+        title: 'แดชบอร์ด',
+        type: 'group',
+        icon: 'heroicons_outline:home',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    // {
+    //     id: 'apps',
+    //     title: 'Apps',
+    //     type: 'group',
+    //     icon: 'heroicons_outline:qr-code',
+    //     children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    // },
+    // {
+    //     id: 'pages',
+    //     title: 'Pages',
+    //     type: 'group',
+    //     icon: 'heroicons_outline:document-duplicate',
+    //     children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    // },
+    // {
+    //     id: 'user-interface',
+    //     title: 'UI',
+    //     type: 'group',
+    //     icon: 'heroicons_outline:rectangle-stack',
+    //     children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    // },
+    // {
+    //     id: 'navigation-features',
+    //     title: 'Misc',
+    //     type: 'group',
+    //     icon: 'heroicons_outline:bars-3',
+    //     children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    // },
+    {
+        id: 'purchase',
+        title: 'ซื้อ',
+        type: 'group',
+        icon: 'heroicons_outline:inbox-arrow-down',
+        children: [],
+    },
+    {
+        id: 'sale',
+        title: 'ขาย',
+        type: 'group',
+        icon: 'heroicons_outline:shopping-cart',
+        children: [],
+    },
+    {
+        id: 'inventory',
+        title: 'คลังสินค้า',
+        type: 'group',
+        icon: 'heroicons_outline:cube',
+        children: [],
+    },
+    {
+        id: 'accounting',
+        title: 'บัญชี/การเงิน',
+        type: 'group',
+        icon: 'heroicons_outline:users',
+        children: [],
+    },
+    {
+        id: 'delivery-workers',
+        title: 'คนส่งของ',
+        type: 'group',
+        icon: 'heroicons_outline:users',
+        children: [],
+    },
+    {
+        id: 'admin',
+        title: 'จัดการพนักงาน',
+        type: 'group',
+        icon: 'heroicons_outline:users',
+        children: [],
+    },
+    {
+        id: 'reports',
+        title: 'รายงาน',
+        type: 'group',
+        icon: 'heroicons_outline:clipboard-document-list',
+        children: [],
+    },
+];
---
+++ src/app/modules/admin/product/form/form.component.ts
@@ -0,0 +1,325 @@
+import { TextFieldModule } from '@angular/cdk/text-field';
+import { NgClass } from '@angular/common';
+import {
+    AfterViewInit,
+    ChangeDetectorRef,
+    Component,
+    OnDestroy,
+    OnInit,
+    ViewEncapsulation,
+} from '@angular/core';
+import {
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    UntypedFormBuilder,
+    Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialog } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FuseConfirmationService } from '@fuse/services/confirmation';
+import { AuthService } from 'app/core/auth/auth.service';
+import { Service } from '../page.service';
+import { CommonModule } from '@angular/common';
+import { NgxDropzoneModule } from 'ngx-dropzone';
+
+@Component({
+    selector: 'form-product',
+    templateUrl: './form.component.html',
+    encapsulation: ViewEncapsulation.None,
+    standalone: true,
+    imports: [
+        MatIconModule,
+        FormsModule,
+        MatFormFieldModule,
+        NgClass,
+        MatInputModule,
+        TextFieldModule,
+        ReactiveFormsModule,
+        MatButtonToggleModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatOptionModule,
+        MatChipsModule,
+        MatDatepickerModule,
+        CommonModule,
+        NgxDropzoneModule,
+    ],
+})
+export class FormComponent implements OnInit, AfterViewInit, OnDestroy {
+    formFieldHelpers: string[] = ['fuse-mat-dense'];
+    fixedSubscriptInput: FormControl = new FormControl('', [
+        Validators.required,
+    ]);
+    dynamicSubscriptInput: FormControl = new FormControl('', [
+        Validators.required,
+    ]);
+    fixedSubscriptInputWithHint: FormControl = new FormControl('', [
+        Validators.required,
+    ]);
+    dynamicSubscriptInputWithHint: FormControl = new FormControl('', [
+        Validators.required,
+    ]);
+
+    item1Data: any = [];
+    item2Data: any = [];
+    itemSupplier: any = [];
+
+    itemBrand: any = [];
+    itemBrandModel: any = [];
+    itemCC: any = [];
+    itemColor: any = [];
+
+    formData: FormGroup;
+    formData2: FormGroup;
+
+    files: File[] = [];
+    warehouseData: any;
+    /**
+     * Constructor
+     */
+    constructor(
+        private _changeDetectorRef: ChangeDetectorRef,
+        private _fuseConfirmationService: FuseConfirmationService,
+        private _formBuilder: FormBuilder,
+        private _Service: Service,
+        private _matDialog: MatDialog,
+        private _router: Router,
+        private _activatedRoute: ActivatedRoute,
+        private _authService: AuthService
+    ) {}
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    async ngOnInit(): Promise<void> {
+        this.formData = this._formBuilder.group({
+            category_product_id: ['', Validators.required],
+            pr_no: [''],
+            name: [''],
+            detail: [''],
+            tank_no: [''],
+            engine_no: [''],
+            license_plate: [''],
+            sale_price: [''],
+            cost: [''],
+            type: [''],
+            year: [''],
+            supplier_id: [''],
+            brand_id: [''],
+            brand_model_id: [''],
+            cc_id: [''],
+            color_id: [''],
+            image: [''],
+            images: [''],
+        });
+
+        this.formData2 = this._formBuilder.group({
+            category_product_id: ['', Validators.required],
+            pr_no: [''],
+            name: [''],
+            detail: [''],
+            tank_no: [''],
+            engine_no: [''],
+            license_plate: [''],
+            sale_price: [''],
+            cost: [''],
+            type: [''],
+            year: [''],
+            supplier_id: [''],
+            brand_id: [''],
+            brand_model_id: [''],
+            cc_id: [''],
+            color_id: [''],
+            image: [''],
+            images: [''],
+        });
+
+        this.getCategories();
+        this.getSuppliers();
+        this.getBrand();
+        this.getBrandModel();
+        this.getCC();
+        this.getColor();
+    }
+
+    /**
+     * After view init
+     */
+    ngAfterViewInit(): void {}
+
+    /**
+     * On destroy
+     */
+    ngOnDestroy(): void {
+        // Unsubscribe from all subscriptions
+    }
+
+    getCategories(): void {
+        this._Service.getCategories().subscribe((resp) => {
+            this.item1Data = resp.data;
+        });
+    }
+
+    getSuppliers(): void {
+        this._Service.getSuppliers().subscribe((resp) => {
+            this.itemSupplier = resp.data;
+        });
+    }
+
+    getBrand(): void {
+        this._Service.getBrand().subscribe((resp) => {
+            this.itemBrand = resp.data;
+        });
+    }
+
+    getBrandModel(): void {
+        this._Service.getBrandModel().subscribe((resp) => {
+            this.itemBrandModel = resp.data;
+        });
+    }
+
+    getCC(): void {
+        this._Service.getCC().subscribe((resp) => {
+            this.itemCC = resp.data;
+        });
+    }
+
+    getColor(): void {
+        this._Service.getColor().subscribe((resp) => {
+            this.itemColor = resp.data;
+        });
+    }
+
+    /**
+     * Get the form field helpers as string
+     */
+    getFormFieldHelpersAsString(): string {
+        return this.formFieldHelpers.join(' ');
+    }
+
+    // somethingChanged(event: any): void {
+    //     this.item2Data = event.value;
+    // }
+
+    somethingBrandChanged(event: any): void {
+        this.itemBrand = event.value;
+    }
+
+    somethingBrandModelChanged(event: any): void {
+        this.itemBrandModel = event.value;
+    }
+
+    somethingCCChanged(event: any): void {
+        this.itemCC = event.value;
+    }
+
+    somethingColorChanged(event: any): void {
+        this.itemColor = event.value;
+    }
+
+    onSelect(event) {
+        this.files.push(...event.addedFiles);
+        // Trigger Image Preview
+        setTimeout(() => {
+            this._changeDetectorRef.detectChanges();
+        }, 150);
+      
+    }
+
+    onRemove(event) {
+        this.files.splice(this.files.indexOf(event), 1);
+
+    }
+
+    New(): void {
+        const confirmation = this._fuseConfirmationService.open({
+            title: 'เพิ่มรายการใหม่',
+            message: 'คุณต้องการเพิ่มรายการใหม่ใช่หรือไม่ ',
+            icon: {
+                show: false,
+                name: 'heroicons_outline:exclamation',
+                color: 'warning',
+            },
+            actions: {
+                confirm: {
+                    show: true,
+                    label: 'ยืนยัน',
+                    color: 'primary',
+                },
+                cancel: {
+                    show: true,
+                    label: 'ยกเลิก',
+                },
+            },
+            dismissible: true,
+        });
+
+        // Subscribe to the confirmation dialog closed action
+        confirmation.afterClosed().subscribe((result) => {
+            // If the confirm button pressed...
+            if (result === 'confirmed') {
+                const formData = new FormData();
+                Object.entries(this.formData.value).forEach(
+                    ([key, value]: any[]) => {
+                        formData.append(key, value);
+                    }
+                );
+
+                Object.entries(this.formData2.value).forEach(
+                    ([key, value]: any[]) => {
+                        formData.append(key, value);
+                    }
+                );
+
+                for (var i = 0; i < this.files.length; i++) {
+                    formData.append('images[]', this.files[i]);
+                }
+
+                this._Service.new(formData).subscribe({
+                    next: (resp: any) => {
+                        this._router
+                            .navigateByUrl('product/list')
+                            .then(() => {});
+                    },
+                    error: (err: any) => {
+                        this._fuseConfirmationService.open({
+                            title: 'กรุณาระบุข้อมูล',
+                            message:
+                                'ไม่สามารถบันทึกข้อมูลได้กรุณาตรวจสอบใหม่อีกครั้ง',
+                            icon: {
+                                show: true,
+                                name: 'heroicons_outline:exclamation',
+                                color: 'warning',
+                            },
+                            actions: {
+                                confirm: {
+                                    show: false,
+                                    label: 'ยืนยัน',
+                                    color: 'primary',
+                                },
+                                cancel: {
+                                    show: false,
+                                    label: 'ยกเลิก',
+                                },
+                            },
+                            dismissible: true,
+                        });
+                    },
+                });
+            }
+        });
+    }
+}
