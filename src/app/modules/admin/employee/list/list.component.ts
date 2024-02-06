45d53455d791b56986d3cad74c9f3fdb34cf5454
Prajchaya <prajehayaRock@gmail.com>
2024-02-06 12:40:42 +0700

    แก้อีกนิดหน่อย

--- src/app/modules/admin/employee/list/list.component.ts
+++ src/app/modules/admin/employee/list/list.component.ts
@@ -105,7 +105,7 @@ export class ListComponent implements OnInit, AfterViewInit {
         // this._router.navigate(['admin/employee/form']);
         const dialogRef = this.dialog.open(FormDialogComponent, {
             width: '1000px',
-            height: '600px', // กำหนดความกว้างของ Dialog
+            height: '800px', // กำหนดความกว้างของ Dialog
         });
 
         dialogRef.afterClosed().subscribe((result) => {
