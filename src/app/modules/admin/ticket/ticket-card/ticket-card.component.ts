08dc167670735b5d09ddc9319e12e460d995be51
narongkorn-kevin <narongkorn.kevin@gmail.com>
2024-03-01 07:38:39 +0700

    ค่อ api หน้า b ooking

--- src/app/modules/admin/ticket/ticket-card/ticket-card.component.ts
+++ src/app/modules/admin/ticket/ticket-card/ticket-card.component.ts
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { DropdownTimeComponent } from 'app/shared/dropdown-time/dropdown-time.component';
 import { PageService } from '../page.service';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-ticket-card',
@@ -17,16 +18,28 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
     styleUrls: ['./ticket-card.component.scss']
 })
 export class TicketCardComponent implements OnInit{
-    serviceData: any[] = []
-    serviceCenterData: any[] = []
+    serviceData: any[] = [];
+    serviceCenterData: any[] = [];
+    form: FormGroup
     constructor(
         private _service: PageService,
         private dialogRef: MatDialogRef<TicketCardComponent>,
         @Inject(MAT_DIALOG_DATA) public data: any,
+        private _fb: FormBuilder
     ) 
     {
 
-        console.log('data', this.data?.id)
+
+        this.form = this._fb.group({
+            client_id: '',
+            car_id: '',
+            phone: '',
+            name: '',
+            service_center_id: '',
+            reason: '',
+            time: '',
+            services: this._fb.array([])
+        })
 
         this._service.getService().subscribe((resp: any)=>{
            this.serviceData = resp.data;
@@ -46,8 +59,28 @@ export class TicketCardComponent implements OnInit{
     onClose() {
         this.dialogRef.close();
     } 
-
-
+    addFeature(serviceId: number) {
+        const service = this.form.get('services') as FormArray;
+        
+        // ตรวจสอบว่า featureId มีอยู่ใน FormArray หรือไม่
+        const index = service.value.findIndex((value: any) => value.service_id === serviceId);
+        console.log(index)
+        if (index === -1) {
+            const value = this._fb.group({
+                service_id: serviceId,
+            }); 
+            service.push(value);
+            console.log(this.form.value)
+           
+        } else {
+          // ถ้ามีอยู่แล้วให้ลบออก
+          service.removeAt(index);
+        }
+      }
+    isChecked(serviceId: number): boolean {
+        const service = this.form.get('services') as FormArray;
+        return service.value.some((value: any) => value.service_id === serviceId);
+      }
 
     
 
